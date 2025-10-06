61c309030812474e5e806a0e7d942b11e7d5a33e
AcheronX. <richardhuynh53@gmail.com>
2025-10-06 13:10:20 -0700

    Finished move and add Texture to

--- src/piece.py
+++ src/piece.py
@@ -1,8 +1,25 @@
-
+import os
 class Piece:
     
-    def __init__(self, name, color,  value, texture, texture_rect=None):
-        pass
+    def __init__(self, name, color,  value, texture, texture_rect = None):
+        self.name = name
+        self.color = color
+
+        value_sign = 1 if color == 'white' else -1
+        self.value = value * value_sign
+        self.set_texture()
+        self.texture = texture
+        self.texture_rect = texture_rect
+        self.moves = []
+        self.moved = False
+
+    def set_texture(self, size = 80):
+        self.texture = os.path.join(
+            f'assets/images/imgs-{size}px/{self.color}_{self.name}.png'
+        )
+    
+    def add_moves(self, move):
+        self.moves.append(move)
 
 class Pawn(Piece):
     def __init__(self, color):
