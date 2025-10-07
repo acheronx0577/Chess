baf8fdac6dfd89f861bec08665480cfacec14c3f
AcheronX. <richardhuynh53@gmail.com>
2025-10-07 15:52:26 -0700

    Make pieces Draggable

--- src/main.py
+++ src/main.py
@@ -15,13 +15,44 @@ class Main:
 
         game = self.game
         screen = self.screen
+        board = self.game.board
+        dragger = self.game.dragger
 
         while True:
             game.show_bg(screen)
             game.show_pieces(screen)
 
+            if dragger.dragging:
+                dragger.update_blit(screen)
+
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                # Click Event
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+                    
+                    clicked_row = dragger.mouseY // SQSIZE
+                    clicked_col = dragger.mouseX // SQSIZE
+
+                    # If clicked square have a piece?
+                    if board.squares[clicked_row][clicked_col].has_piece():
+                        piece = board.squares[clicked_row][clicked_col].piece
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
+
+                # Mouse Motion
+                elif event.type == pygame.MOUSEMOTION:
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        game.show_bg(screen)
+                        game.show_pieces(screen)
+                        dragger.update_blit(screen)
+
+                # Click Release
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    dragger.undrag_piece()
+
+                # Quit Application
+                elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             pygame.display.update()
